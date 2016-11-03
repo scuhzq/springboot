fd24f11ad4e80bded2fbf598d92eda7555b7afa5
hzq <1427838666@qq.com>
2016-11-03 12:02:55 +0800

    springboot 项目部署到tomcat中

--- src/main/java/com/he/Application.java
+++ src/main/java/com/he/Application.java
@@ -2,12 +2,20 @@ package com.he;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by hzq on 16/10/27.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
