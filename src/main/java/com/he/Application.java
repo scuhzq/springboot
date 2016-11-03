154a0dd5811442cd64b6b5634d363bd7e8e37494
hzq <1427838666@qq.com>
2016-11-03 16:52:44 +0800

    application继承类

--- src/main/java/com/he/Application.java
+++ src/main/java/com/he/Application.java
@@ -3,7 +3,7 @@ package com.he;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by hzq on 16/10/27.
